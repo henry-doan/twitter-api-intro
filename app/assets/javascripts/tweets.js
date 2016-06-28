9b51f2a204191f87247818159b864bbc06198977
Henry Doan <henrydoan96@gmail.com>
2016-06-28 12:18:43 -0600

    end of lecture

---
+++ app/assets/javascripts/tweets.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+	$('#tweet_button').on('click', function() {
+		var tweet = $('#tweet').val();
+		$.ajax({
+			url: '/tweets',
+			type: 'POST',
+			data: { tweet: tweet }
+		}).done( function(tweet) {
+			$('#tweets').append('<li>' + tweet.text + '</li>')
+		}).fail( function() {
+			alert('Did not add')
+		})
+	})
+});
